551b51c0a8337d5220cef41f18be21b5d0cb44c0
Rodrigo Oliveira <projetosrodrigojava@gmail.com>
2023-10-17 11:21:41 -0300

    API de gestao de vendas com Spring Boot e Docker

---
+++ src/test/java/com/gvendas/gestavendas/GestaVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gvendas.gestavendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
